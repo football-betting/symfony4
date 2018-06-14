f61460894a16d839991fed51f8ac39f525613e4f
Rafal Wesolowski <wesolowski@nexus-netsoft.com>
2018-06-14 22:27:57 +0200

    Add Point Collection

---
+++ src/GameRating/Business/GamePoints/Config.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace App\GameRating\Business\GamePoints;
+
+
+interface Config
+{
+    public const WIN_EXACT = 3;
+    public const WIN_SCORE_DIFF = 2;
+    public const WIN_TEAM = 1;
+    public const NO_WIN_TEAM = 0;
+}
