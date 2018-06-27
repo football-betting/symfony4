13ff8d86e35ea4027bb4d3cc86f2de0449f75094
Steven Zemelka <steven.zemelka@gmail.com>
2018-06-28 00:49:24 +0200

    docblocks added and removed inline comments

--- src/User/Communication/Controller/Registration.php
+++ src/User/Communication/Controller/Registration.php
@@ -20,32 +20,28 @@ class Registration extends Controller
      */
     public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder)
     {
-        // 1) build the form
         $user = new User();
         $form = $this->createForm(UserType::class, $user);
 
-        // 2) handle the submit (will only happen on POST)
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
 
-            // 3) Encode the password (you could also do this via Doctrine listener)
             $password = $passwordEncoder->encodePassword($user, $user->getPlainPassword());
             $user->setPassword($password);
 
-            // 4) save the User!
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($user);
             $entityManager->flush();
 
-            // ... do any other work - like sending them an email, etc
-            // maybe set a "flash" success message for the user
-
             return $this->redirectToRoute('login');
         }
+        $form->getErrors();
 
         return $this->render(
             'user/registration/register.html.twig',
-            array('form' => $form->createView())
+            [
+                'form' => $form->createView()
+            ]
         );
     }
 }
